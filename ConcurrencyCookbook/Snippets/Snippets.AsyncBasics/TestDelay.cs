fdb3e8ed0a6927989d42bfb3392bd0d6a83689fd
Carlos Anderson <carlosjanderson@gmail.com>
2019-07-27 21:13:27 -0400

    Add initial solution and snippet

---
+++ ConcurrencyCookbook/Snippets/Snippets.AsyncBasics/TestDelay.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Snippets.AsyncBasics
+{
+    [TestFixture]
+    public class TestDelay
+    {
+        [Test]
+        public async Task ServiceSucceeds()
+        {
+            // In a real scenario, I wouldn't be testing that the service succeeds;
+            // I'd be testing something else, which uses the service as part of the test
+
+            var service = new FakeService();
+
+            var result = await service.ExecuteAsync();
+
+            Assert.That(result, Is.EqualTo(42));
+        }
+    }
+
+    public interface ISomeService
+    {
+        Task<int> ExecuteAsync();
+    }
+
+    // Create a fake service that returns fixed results after a delay
+    public class FakeService : ISomeService
+    {
+        public Task<int> ExecuteAsync()
+        {
+            // Succeeds asynchronously by returning the given result after 1 second
+            return TestHelpers.DelayResult(42, TimeSpan.FromSeconds(1));
+        }
+    }
+
+    public class TestHelpers
+    {
+        // From recipe 2.1
+        public static async Task<T> DelayResult<T>(T result, TimeSpan delay)
+        {
+            await Task.Delay(delay);
+            return result;
+        }
+    }
+}
