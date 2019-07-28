2a264c03d3d1f7337b76dc57861d6640564f6466
Carlos Anderson <carlosjanderson@gmail.com>
2019-07-28 01:55:47 -0400

    Test Task.WhenAny, and enhance WPF progress bars

--- ConcurrencyCookbook/Snippets/Snippets.AsyncBasics.Wpf/MainWindow.xaml.cs
+++ ConcurrencyCookbook/Snippets/Snippets.AsyncBasics.Wpf/MainWindow.xaml.cs
@@ -1,52 +1,101 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Snippets.AsyncBasics.Wpf
 {
     public partial class MainWindow : Window
     {
+        public const int TaskCount = 10;
+
+        public readonly int[] TaskRange = Enumerable.Range(0, TaskCount).ToArray();
+
+        private readonly ProgressBar[] _progressBars;
+
         private CancellationTokenSource _cts;
-        private Service _service;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            _progressBars = TaskRange.Select(_ => new ProgressBar
+            {
+                Minimum = 0, Maximum = 99,
+                Height = 24
+            }).ToArray();
+
+            ProgressBars.ItemsSource = _progressBars;
         }
 
         private async void StartButton_OnClick(object sender, RoutedEventArgs e)
         {
-            _service = new Service();
+            _cts = new CancellationTokenSource();
 
-            // Update the progress bar upon progress report
-            var progress = new Progress<int>(i => ProgressBar.Value = i);
+            // Generate random delays in milliseconds
+            var random = new Random();
+            var delaysInMs = TaskRange.Select(_ => random.Next(10, 50)).ToArray();
+
+            // Create services that take as long as each generated delay
+            var services = TaskRange.Select(i => new Service(TimeSpan.FromMilliseconds(delaysInMs[i]))).ToArray();
+
+            // Create tasks that execute each service
+            var tasks = TaskRange.Select(i =>
+            {
+                var progress = new Progress<int>(p => _progressBars[i].Value = p);
+                return services[i].ExecuteAsync(progress, _cts.Token);
+            }).ToArray();
+
+
+            // Create tasks that do something as soon as each completes
+            var processingTasks = tasks.Select(async (t, i) =>
+            {
+                await t;
+                _progressBars[i].Foreground = Brushes.Blue;
+            }).ToArray();
 
             try
             {
-                _cts = new CancellationTokenSource();
-                await _service.ExecuteAsync(progress, _cts.Token);
+                // Wait for all the tasks to complete
+                await Task.WhenAll(processingTasks);
             }
             catch (TaskCanceledException)
             {
-                ProgressBar.Value = 0;
+                // If any task was cancelled, reset all progress bars
+                // Note: all tasks are cancelled because they share the cancellation token
+                foreach (var progressBar in _progressBars)
+                {
+                    progressBar.Value = 0;
+                    progressBar.Foreground = Brushes.Green;
+                }
             }
         }
 
         private void CancelButton_OnClick(object sender, RoutedEventArgs e)
         {
+            // Cancel all the tasks (because they all use the same token)
             _cts.Cancel();
         }
     }
 
     public class Service
     {
+        private readonly TimeSpan _delay;
+
+        public Service(TimeSpan delay)
+        {
+            _delay = delay;
+        }
+
         public async Task ExecuteAsync(IProgress<int> progress, CancellationToken cancellationToken)
         {
             for (int i = 0; i < 100; i++)
             {
                 progress?.Report(i);
-                await Task.Delay(TimeSpan.FromMilliseconds(100), cancellationToken);
+                await Task.Delay(_delay, cancellationToken);
             }
         }
     }
---
+++ ConcurrencyCookbook/Snippets/Snippets.AsyncBasics/TestWhenAny.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Snippets.AsyncBasics
+{
+    [TestFixture]
+    public class TestWhenAny
+    {
+        [Test]
+        public async Task ReturnQuickestResult()
+        {
+            var random = new Random();
+            var randomTimesInMs = Enumerable.Range(0, 100).Select(x => random.Next(100, 1000)).ToArray();
+
+            // Create tasks that are delayed by the random time,
+            // and whose integer result is that random time itself
+            var tasks = randomTimesInMs.Select(t => TestHelpers.DelayResult(t, TimeSpan.FromMilliseconds(t)));
+
+            // Start tasks
+            var taskArray = tasks.ToArray();
+
+            // Get the first task to complete
+            var firstTask = await Task.WhenAny(taskArray);
+
+            // Retrieve the task's value
+            // Note: Consider cancelling other tasks if they're resource-intensive because they're still running
+            var result = await firstTask;
+
+            // The result should be the the minimum time (because it's the quickest to finish)
+            // Note: However, tests show this isn't always true: sometimes the result
+            // is close to the minimum, but not exact; probably due to overhead of starting tasks
+            Assert.That(result, Is.EqualTo(randomTimesInMs.Min()));
+        }
+
+        [Test]
+        public async Task TestTaskWithException()
+        {
+            var taskWithException = Task.FromException<int>(new Exception());
+            var normalTask = TestHelpers.DelayResult(42, TimeSpan.FromSeconds(1));
+
+            // The first task (with exception) won't cause an exception here
+            var firstTask = await Task.WhenAny(taskWithException, normalTask);
+
+            try
+            {
+                // Try to retrieve the task's value (but there's an exception)
+                var result = await firstTask;
+
+                // Should never get here because above should throw an exception
+                Assert.Fail();
+            }
+            catch (Exception)
+            {
+                Assert.Pass();
+            }
+        }
+    }
+}
