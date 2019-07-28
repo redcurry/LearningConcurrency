1654265325f375a321ba690def34238ed7c539be
Carlos Anderson <carlosjanderson@gmail.com>
2019-07-27 23:33:20 -0400

    Add WPF project to test progress and cancellation

---
+++ ConcurrencyCookbook/Snippets/Snippets.AsyncBasics.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Snippets.AsyncBasics.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        private CancellationTokenSource _cts;
+        private Service _service;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void StartButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _service = new Service();
+
+            // Update the progress bar upon progress report
+            var progress = new Progress<int>(i => ProgressBar.Value = i);
+
+            try
+            {
+                _cts = new CancellationTokenSource();
+                await _service.ExecuteAsync(progress, _cts.Token);
+            }
+            catch (TaskCanceledException)
+            {
+                ProgressBar.Value = 0;
+            }
+        }
+
+        private void CancelButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            _cts.Cancel();
+        }
+    }
+
+    public class Service
+    {
+        public async Task ExecuteAsync(IProgress<int> progress, CancellationToken cancellationToken)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                progress?.Report(i);
+                await Task.Delay(TimeSpan.FromMilliseconds(100), cancellationToken);
+            }
+        }
+    }
+}
